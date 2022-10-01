e8ae084756db4b8db726deb6fccd5f178f83325a
Kauã Steliano <stelianok@gmail.com>
2022-10-01 09:22:06 -0300

    Downloaded necessary dependencies

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {
+        <View>
+          <Text>Pato</Text>
+        </View>
+      }
+    </NavigationContainer>
+  );
+}
