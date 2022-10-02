f2159b8ba0f6bbd70fff81cd10c49251d071cfae
Kauã Steliano <stelianok@gmail.com>
2022-10-02 15:06:53 -0300

    Finished Second OnBoarding Screen

--- src/screens/OnBoarding/SecondBoard/index.tsx
+++ src/screens/OnBoarding/SecondBoard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import {Container} from './styles';
-import {Heading1} from '../../../GlobalStyles';
+import {
+  Container,
+  Background,
+  Body,
+  StarsContainer,
+  Stars,
+  Title,
+  TitleBlue,
+  SolarSystem,
+  TapToContinue,
+} from './styles';
+
+import SolarSystemImg from './../../../../assets/images/OnBoarding/SolarSystem.png';
 
 export default function SecondBoard() {
   return (
     <Container>
-      <Heading1>XD</Heading1>
+      <Background>
+        <StarsContainer>
+          <Stars />
+        </StarsContainer>
+        <Body>
+          <Title>
+            A spacecraft that uses{' '}
+            <TitleBlue>breakthrough technology and autonomy</TitleBlue> to
+            endure <TitleBlue>heat and radiation</TitleBlue> like no other
+            mission
+          </Title>
+          <SolarSystem resizeMode={'contain'} source={SolarSystemImg} />
+          <TapToContinue>Tap to continue</TapToContinue>
+        </Body>
+      </Background>
     </Container>
   );
 }
--- src/screens/OnBoarding/SecondBoard/styles.ts
+++ src/screens/OnBoarding/SecondBoard/styles.ts
@@ -1,6 +1,54 @@
 import styled from 'styled-components/native';
+import { Heading2, Heading3, Heading4 } from '../../../GlobalStyles';
+
+import StarsSVG from '../../../../assets/images/StarsBackMobile.svg';
+import StyleGuide from '../../../StyleGuide';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: black;
+`;
+export const Background = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: ${StyleGuide.colors.dark.dark_4};
+`;
+
+export const StarsContainer = styled.SafeAreaView`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 35px;
+  margin: auto;
+`;
+
+export const Stars = styled(StarsSVG)`
+  width: 45,
+  height: 45,
+  `;
+
+export const Body = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: space-around;
+  margin-horizontal: 34px;
+  margin-top: 32px;
+`;
+
+export const Title = styled(Heading3)`
+  color: ${StyleGuide.colors.brand.white};
+  text-align: center;
+`;
+export const TitleBlue = styled(Heading3)`
+  color: ${StyleGuide.colors.brand.blue};
+  text-align: center;
+`;
+export const SolarSystem = styled.Image`
+  margin-top: -170px;
+`;
+export const TapToContinue = styled(Heading4)`
+  color: ${StyleGuide.colors.brand.white};
+  text-align: center;
+  padding-bottom: 60px;
 `;
