a22338db0c653b4266fe3b4b81f1b644aca16912
Kauã Steliano <stelianok@gmail.com>
2022-10-02 03:11:41 -0300

    almost  finishing introduction

--- src/screens/IntroScreen/index.tsx
+++ src/screens/IntroScreen/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import {Container} from './styles';
-import {Heading1} from '../../GlobalStyles';
 
+import AstronomerPic from '../../../assets/images/OnBoarding/Astronomer.png';
+import {
+  Container,
+  StarsContainer,
+  Stars,
+  Background,
+  Body,
+  Title,
+  Astronomer,
+  Button,
+  Row,
+  ButtonText,
+  WrinklySunIcon,
+} from './styles';
+
+import {Heading2} from '../../GlobalStyles';
+import StyleGuide from '../../StyleGuide';
 export default function IntroScreen() {
   return (
     <Container>
-      <Heading1>XD</Heading1>
+      <Background>
+        <StarsContainer>
+          <Stars />
+        </StarsContainer>
+        <Body>
+          <Title>
+            Let’s Explore the{' '}
+            <Title style={{color: StyleGuide.colors.brand.blue}}>Sun</Title>{' '}
+            with{' '}
+            <Title style={{color: StyleGuide.colors.brand.blue}}>
+              Parker Solar Probe
+            </Title>
+          </Title>
+          <Astronomer source={AstronomerPic} />
+          <Button>
+            <Row>
+              <ButtonText>Let's Go!</ButtonText>
+              <WrinklySunIcon />
+            </Row>
+          </Button>
+        </Body>
+      </Background>
     </Container>
   );
 }
--- src/screens/IntroScreen/styles.ts
+++ src/screens/IntroScreen/styles.ts
@@ -1,7 +1,46 @@
 import styled from 'styled-components/native';
 import StyleGuide from '../../StyleGuide';
+import { RectButton } from 'react-native-gesture-handler';
 
+import StarsSVG from '../../../assets/images/planets/Stars.svg';
+import SunWrinkly from '../../../assets/images/OnBoarding/SunWrinkly.svg';
+import { Heading2, Heading4 } from '../../GlobalStyles';
 export const Container = styled.SafeAreaView`
   flex: 1;
+`;
+
+export const StarsContainer = styled.SafeAreaView`
+  position: absolute;
+`;
+
+export const Stars = styled(StarsSVG)`
+  width: 25,
+  height: 25,
+  `;
+
+export const Background = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
   background-color: ${StyleGuide.colors.dark.dark_4};
+`;
+export const Body = styled.View`
+  margin-horizontal: 34px;
+  margin-top: 75px;
+`;
+
+export const Title = styled(Heading2)`
+  color: ${StyleGuide.colors.brand.white};
+  text-align: center;
+`;
+
+export const Astronomer = styled.Image``;
+export const Button = styled(RectButton)``;
+export const Row = styled.View`
+  flex-direction: row;
+`;
+export const ButtonText = styled(Heading4)``;
+export const WrinklySunIcon = styled(SunWrinkly)`
+width: 25,
+height: 25,
 `;
