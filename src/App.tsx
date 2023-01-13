4bbe32b7082b5e15f60e4c862635c03ecefb4d36
Kauã Steliano <stelianok@gmail.com>
2023-01-13 09:45:01 -0300

    Added Bracket Spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import StackNavigation from './navigations/StackNavigation';
 
