0c1bd185632ae1c761766be84153f01dcfe3078b
Kauã Steliano <stelianok@gmail.com>
2023-01-13 10:34:04 -0300

    Created Component to represent SpaceAppsLogo

---
+++ src/components/SpaceAppsLogo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import SpaceAppsLogoPic from '../../../assets/images/SpaceAppsLogo.png';
+
+import { SpaceAppsLogoContainer } from './styles';
+export default function SpaceAppsLogo() {
+  return <SpaceAppsLogoContainer source={SpaceAppsLogoPic} />;
+}
---
+++ src/components/SpaceAppsLogo/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const SpaceAppsLogoContainer = styled.Image`
+  width: 110px;
+  height: 110px;
+`;
